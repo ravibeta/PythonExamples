be7ab32a0b1410759ff656c6fe556ef54f8fea4a
Ravi Rajamani <ravibeta@hotmail.com>
2012-06-18 18:12:02 -0700

    KosmosFS application

--- KFS/KFS/KFS.py
+++ KFS/KFS/KFS.py
@@ -1,8 +1,22 @@
-f = open('/tmp/workfile','w')
-print f
-f.write('Hello World\n')
-f.close()
-with open('/tmp/workfile', 'r') as r:
-	for line in r:
-		print line
-r.closed
+import pickle
+l = ('abc','defg','hijk','lmno','pqrs','tuvw','xyza','bcd','efg')
+class FileRWTester:
+     def ReadWrite(self):    
+        name = raw_input("File name with path: ")
+        with open(name,'w') as f:
+            del f
+        with open(name,'w') as f:
+            pickle.dump(l,f)
+        with open(name, 'r') as r:
+            u = pickle.load(r)
+            print u
+            for word in u:
+                print word
+        r.closed
+
+fi = FileRWTester()
+fi.ReadWrite()
+
+
+
+
