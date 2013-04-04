84cc8db7841c937213a2d810427e95a3b3dea07c
Ravi Rajamani <ravibeta@hotmail.com>
2013-04-04 07:11:20 -0700

    Update TextIndexing.py

--- TextIndexing/TextIndexing/TextIndexing.py
+++ TextIndexing/TextIndexing/TextIndexing.py
@@ -179,3 +179,21 @@ def indexText(text):
     print "----------------"
 
 indexText('Clustering and Segmentation. Clustering is a data mining technique that is directed towards the goals of identification and classification. Clustering tries to identify a finite set of categories or clusters to which each data object (tuple) can be mapped. The categories may be disjoint or overlapping and may sometimes be organized into trees. For example, one might form categories of customers into the form of a tree and then map each customer to one or more of the categories. A closely related problem is that of estimating multivariate probability density functions of all variables that could be attributes in a relation or from different relations.')
+
+from nltk.corpus import brown
+ suffix_fdist = nltk.FreqDist()
+ common_suffixes = suffix_fdist.keys()[:100]
+ 
+# use pos to cleanup initial list 
+# def pos_features(word):
+#          features = {}
+#               for suffix in common_suffixes:
+#                            features['endswith(%s)' % suffix] = word.lower().endswith(suffix)
+#                            
+#                            tagged_words = brown.tagged_words(categories='news')
+#                            featuresets = [(pos_features(n), g) for (n,g) in tagged_words]
+#                            size = int(len(featuresets) * 0.1)
+#                            train_set, test_set = featuresets[size:], featuresets[:size]
+#                            classifier = nltk.DecisionTreeClassifier.train(train_set)
+#                            classifier.classify(pos_features('cats'))
+#                            'NNS'
