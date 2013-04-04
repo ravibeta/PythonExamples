4600294977cf11ff7235468afb19a25f9a8c98ad
Ravi Rajamani <ravibeta@hotmail.com>
2013-04-04 07:21:38 -0700

    Update TextIndexing.py

--- TextIndexing/TextIndexing/TextIndexing.py
+++ TextIndexing/TextIndexing/TextIndexing.py
@@ -180,9 +180,9 @@ def indexText(text):
 
 indexText('Clustering and Segmentation. Clustering is a data mining technique that is directed towards the goals of identification and classification. Clustering tries to identify a finite set of categories or clusters to which each data object (tuple) can be mapped. The categories may be disjoint or overlapping and may sometimes be organized into trees. For example, one might form categories of customers into the form of a tree and then map each customer to one or more of the categories. A closely related problem is that of estimating multivariate probability density functions of all variables that could be attributes in a relation or from different relations.')
 
-from nltk.corpus import brown
- suffix_fdist = nltk.FreqDist()
- common_suffixes = suffix_fdist.keys()[:100]
+#from nltk.corpus import brown
+# suffix_fdist = nltk.FreqDist()
+# common_suffixes = suffix_fdist.keys()[:100]
  
 # use pos to cleanup initial list 
 # def pos_features(word):
