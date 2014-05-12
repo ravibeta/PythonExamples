f663b9572a88919d313d173697ee0d3e84ca5311
Ravi Rajamani <ravibeta@hotmail.com>
2014-05-12 01:20:26 -0700

    Update DirectReports.py

--- DirectReports.py
+++ DirectReports.py
@@ -6,7 +6,7 @@ class Employee:
     
 # sample data
 #E = [Employee(1, 0, 'ABC'), Employee(2, 1, 'DEF'), Employee(3, 1, 'GHI')]
-E = [Employee(1, 0, 'ABC'), Employee(4,0,'IJK'), Employee(2, 1, 'DEF'), Employee(5,4,'MNO'), Employee(3, 1, 'GHI')]
+E = [Employee(1, 1, 'ABC'), Employee(4,4,'IJK'), Employee(2, 1, 'DEF'), Employee(5,4,'MNO'), Employee(3, 1, 'GHI')]
 
 def directReportsBFS(E,s):
 	Q = []
@@ -17,10 +17,10 @@ def directReportsBFS(E,s):
 		if c is None:
 			level = level + 1
 			continue
-		Reports = [e for e in E if e.mgrId == c.id]
+		Reports = [e for e in E if e.mgrId == c.id and e.mgrId != e.id]
 		print 'Name:' + c.name + ' Level:' + str(level) + ' Reports:' +"[{0}]".format(", ".join(str(r.name) for r in Reports))
 		Q.append(None)
 		all(Q.append(report) for report in Reports)
 
 
-[directReportsBFS(E,e) for e in E if e.mgrId == 0]
+[directReportsBFS(E,e) for e in E if e.mgrId == e.id]
