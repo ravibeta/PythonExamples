d71cf7817ec16eaca70e4775e995aebac82cb7ae
Ravi Rajamani <ravibeta@hotmail.com>
2014-05-12 00:40:06 -0700

    Update DirectReports.py

--- DirectReports.py
+++ DirectReports.py
@@ -7,17 +7,7 @@ class Employee:
 # sample data
 E = [Employee(1, 0, 'ABC'), Employee(2, 1, 'DEF'), Employee(3, 1, 'GHI')]
 
-# method 1
-def directReports(E, c, level):
-	    c.level = level
-	    Reports = [e for e in E if e.mgrId == c.id]
-	    c.reports = Reports
-	    print 'Name:' + c.name + ' Level:' + str(c.level) + ' Reports:' +"[{0}]".format(", ".join(str(r.name) for r in Reports))
-	    all(directReports(E,report,level+1) for report in Reports)
 
-all(directReports(E,c,0) for c in E if c.mgrId == 0)
-
-# method 2
 def directReportsBFS(E):
 	Q = []
 	level = 0
@@ -27,9 +17,7 @@ def directReportsBFS(E):
 		if c is None:
 			level = level + 1
 			continue
-		c.level = level;
 		Reports = [e for e in E if e.mgrId == c.id]
-		c.reports = Reports
 		print 'Name:' + c.name + ' Level:' + str(c.level) + ' Reports:' +"[{0}]".format(", ".join(str(r.name) for r in Reports))
 		Q.append(None)
 		all(Q.append(report) for report in Reports)
