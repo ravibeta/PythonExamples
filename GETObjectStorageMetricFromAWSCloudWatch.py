baf53c8ccf58da5c101d43d666f34a199fcfbca4
Ravi Rajamani <ravibeta@hotmail.com>
2018-12-30 13:21:03 -0800

    Update GETObjectStorageMetricFromAWSCloudWatch.py

--- GETObjectStorageMetricFromAWSCloudWatch.py
+++ GETObjectStorageMetricFromAWSCloudWatch.py
@@ -1,206 +1,159 @@
-# Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# ADAPATED TO GET ON-PREMISE OBJECT STORAGE METRICS BY Ravi Rajamani
-#
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of the
-# License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
+package com.emc.ecs.s3.sample;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
-# AWS Version 4 signing example
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
-# Monitoring API (Monitoring)
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-# See: http://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
-# This version makes a POST request and passes request parameters
-# in the body (payload) of the request. Auth information is passed in
-# an Authorization header.
-import sys, os, base64, datetime, hashlib, hmac
-import requests # pip install requests
+public class GetMetricsRequest {
+    private static final Logger logger = LoggerFactory.getLogger(GetMetricsRequest.class);
 
-# ************* REQUEST VALUES *************
-method = 'POST'
-service = os.environ.get('AWS_service')
-host = os.environ.get('AWS_host')
-region = os.environ.get('AWS_region')
-endpoint = os.environ.get('AWS_endpoint')
-request_parameters = os.environ.get('AWS_request_parameters')
-apiName='GetMetricStatistics'
-# PUT requests use a content type header. For Monitoring API,
-# the content is JSON.
-content_type = 'application/x-amz-json-1.0'
-# Monitoring API requires an x-amz-target header
-amz_target = 'GraniteServiceVersion20100801.'+apiName
+    public static byte[] computeHmacSHA256(byte[] key, String data) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException,
+            UnsupportedEncodingException {
+        String algorithm = "HmacSHA256";
+        String charsetName = "UTF-8";
 
-# Request parameters for CreateTable--passed in a JSON block.
-t = datetime.datetime.utcnow()
-request_parameters = '{'
-request_parameters += '    "Action": "GetMetricStatistics", '
-request_parameters += '    "Namespace": "On-PremiseObjectStorageMetrics",'
-request_parameters += '    "MetricName": "BucketSizeBytes ",'
-request_parameters += '    "Dimensions": ['
-request_parameters += '        {'
-request_parameters += '            "Name": "BucketName",'
-request_parameters += '            "Value": "ExampleBucket"'
-request_parameters += '        }'
-request_parameters += '    ],'
-request_parameters += '    "StartTime": 1545884562,'
-request_parameters += '    "EndTime":  1545884662,'
-#request_parameters += '    "StartTime": 2018-12-25T23:00:00Z,'
-#request_parameters += '    "EndTime": 2018-12-27T23:00:00Z,'
-request_parameters += '    "Period": 86400,'
-request_parameters += '    "Statistics": ['
-request_parameters += '        "Average"'
-request_parameters += '    ],'
-request_parameters += '    "Unit": "Bytes"'
-request_parameters += '}'
-"""
-request_parameters = '{'
-request_parameters += '"Dimensions.member.1.Value": "ExampleBucket", '
-request_parameters += '"MetricName": "BucketSizeBytes", '
-request_parameters += '"Period": 86400, '
-request_parameters += '"Version": "2010-08-01",'
-request_parameters += '"Statistics.member.1": "Average",'
-request_parameters += '"EndTime": 2018-12-27T23:00:00Z,'
-request_parameters += '"StartTime": 2018-12-25T23:00:00Z,'
-request_parameters += '"Dimensions.member.1.Name": "BucketName",'
-request_parameters += '"Action": "GetMetricStatistics",'
-request_parameters += '"Namespace": "On-PremiseObjectStorageMetrics"'
-request_parameters += '}'
-"""
-print(request_parameters)
+        Mac sha256_HMAC = Mac.getInstance(algorithm);
+        SecretKeySpec secret_key = new SecretKeySpec(key, algorithm);
+        sha256_HMAC.init(secret_key);
 
+        return sha256_HMAC.doFinal(data.getBytes(charsetName));
+    }
 
-# Key derivation functions. See:
-# http://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-python
-def sign(key, msg):
-    return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+    public static byte[] computeHmacSHA256(String key, String data) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException,
+            UnsupportedEncodingException {
+        return computeHmacSHA256(key.getBytes(), data);
+    }
 
-def getSignatureKey(key, date_stamp, regionName, serviceName):
-    kDate = sign(('AWS4' + key).encode('utf-8'), date_stamp)
-    kRegion = sign(kDate, regionName)
-    kService = sign(kRegion, serviceName)
-    kSigning = sign(kService, 'aws4_request')
-    return kSigning
+    public static String getSignatureV4(String accessSecretKey, String date, String region, String regionService, String signing, String stringToSign)
+            throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException {
 
-# Read AWS access key from env. variables or configuration file. Best practice is NOT
-# to embed credentials in code.
-access_key = os.environ.get('AWS_ACCESS_KEY_ID')
-secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-if access_key is None or secret_key is None:
-    print('No access key is available.')
-    sys.exit()
+        byte[] dateKey = computeHmacSHA256(accessSecretKey, date);
+        logger.debug("dateKey: {}", encodeToString(dateKey));
 
-# Create a date for headers and the credential string
-amz_date = t.strftime('%Y%m%dT%H%M%SZ')
-date_stamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+        byte[] dateRegionKey = computeHmacSHA256(dateKey, region);
+        logger.debug("dateRegionKey: {}", encodeToString(dateRegionKey));
 
+        byte[] dateRegionServiceKey = computeHmacSHA256(dateRegionKey, regionService);
+        logger.debug("dateRegionServiceKey: {}", encodeToString(dateRegionServiceKey));
 
-# ************* TASK 1: CREATE A CANONICAL REQUEST *************
-# http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+        byte[] signingKey = computeHmacSHA256(dateRegionServiceKey, signing);
+        logger.debug("signingKey: {}", encodeToString(signingKey));
 
-# Step 1 is to define the verb (GET, POST, etc.)--already done.
+        byte[] signature = computeHmacSHA256(signingKey, stringToSign);
+        logger.debug("signature: {}", encodeToString(signature));
 
-# Step 2: Create canonical URI--the part of the URI from domain to query
-# string (use '/' if no path)
-canonical_uri = '/'
+        return encodeToString(signature);
+    }
+    private static final char[] DIGITS = {
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+    public static String encodeToString(byte[] bytes) {
+        char[] encodedChars = encode(bytes);
+        return new String(encodedChars);
+    }
+    public static char[] encode(byte[] data) {
 
-## Step 3: Create the canonical query string. In this example, request
-# parameters are passed in the body of the request and the query string
-# is blank.
-canonical_querystring = ''
+        int l = data.length;
 
-# Step 4: Create the canonical headers. Header names must be trimmed
-# and lowercase, and sorted in code point order from low to high.
-# Note that there is a trailing \n.
-canonical_headers = 'content-type:' + content_type + '\n' + 'host:' + host + '\n' + 'x-amz-date:' + amz_date + '\n' + 'x-amz-target:' + amz_target + '\n'
+        char[] out = new char[l << 1];
 
-# Step 5: Create the list of signed headers. This lists the headers
-# in the canonical_headers list, delimited with ";" and in alpha order.
-# Note: The request can include any headers; canonical_headers and
-# signed_headers include those that you want to be included in the
-# hash of the request. "Host" and "x-amz-date" are always required.
-signed_headers = 'content-type;host;x-amz-date;x-amz-target'
-
-# Step 6: Create payload hash. In this example, the payload (body of
-# the request) contains the request parameters.
-payload_hash = hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
-
-# Step 7: Combine elements to create canonical request
-canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
-
-
-# ************* TASK 2: CREATE THE STRING TO SIGN*************
-# Match the algorithm to the hashing algorithm you use, either SHA-1 or
-# SHA-256 (recommended)
-algorithm = 'AWS4-HMAC-SHA256'
-credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
-string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
-
-# ************* TASK 3: CALCULATE THE SIGNATURE *************
-# Create the signing key using the function defined above.
-signing_key = getSignatureKey(secret_key, date_stamp, region, service)
-
-# Sign the string_to_sign using the signing_key
-signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-
-
-# ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
-# Put the signature information in a header named Authorization.
-authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
-
-# "x-amz-target", "content-type", and "Authorization". Except for the authorization
-# header, the headers must be included in the canonical_headers and signed_headers values, as
-# noted earlier. Order here is not significant.
-# # Python note: The 'host' header is added automatically by the Python 'requests' library.
-#headers = {'Content-Type':content_type,
-#           'X-Amz-Date':amz_date,
-#           'X-Amz-Target':amz_target,
-#           'Authorization':authorization_header}
-headers = {'x-amz-date':amz_date,
-           'Authorization':authorization_header,
-           'x-amz-target':'GraniteServiceVersion20100801.'+apiName,
-           'Content-Type': content_type,
-           'Accept': 'application/json',
-           'Content-Encoding': 'amz-1.0',
-           'Connection': 'keep-alive'}
-
-
-
-# ************* SEND THE REQUEST *************
-print('\nBEGIN REQUEST++++++++++++++++++++++++++++++++++++')
-print('Request URL = ' + endpoint)
-
-r = requests.post(endpoint, data=request_parameters, headers=headers)
-
-print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
-print('Response code: %d\n' % r.status_code)
-print(r.text)
-
-
-"""
-{    "Action": "GetMetricStatistics",     "Namespace": "On-PremiseObjectStorageMetrics",    "MetricName": "BucketSizeBytes",    "Dimensions": [        {            "Name": "BucketName",            "Value": "ExampleBucket"        }    ],    "StartTime": 1545884562,    "EndTime": 1545884662,    "Period": 86400,    "Statistics": [        "Average"    ],    "Unit": "Bytes"}
-
-BEGIN REQUEST++++++++++++++++++++++++++++++++++++
-Request URL = https://monitoring.us-east-1.amazonaws.com
-
-RESPONSE++++++++++++++++++++++++++++++++++++
-Response code: 200
-
-{"Datapoints":[{"Average":1.024E12,"Timestamp":1.54588452E9,"Unit":"Bytes"}],"Label":"BucketSizeBytes"}
-aws cloudwatch get-metric-statistics --metric-name BucketSizeBytes --start-time 2018-12-25T23:00:00 --end-time 2018-12-27T23:00:00 --period 86400 --namespace On-PremiseObjectStorageMetrics --statistics Average --dimensions Name=BucketName,Value=ExampleBucket
-{
-    "Label": "BucketSizeBytes",
-    "Datapoints": [
-        {
-            "Timestamp": "2018-12-26T23:00:00Z",
-            "Average": 1024000000000.0,
-            "Unit": "Bytes"
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = DIGITS[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS[0x0F & data[i]];
         }
-    ]
+
+        return out;
+    }
+    protected static int toDigit(char ch, int index) throws IllegalArgumentException {
+        int digit = Character.digit(ch, 16);
+        if (digit == -1) {
+            throw new IllegalArgumentException("Illegal hexadecimal character " + ch + " at index " + index);
+        }
+        return digit;
+    }
+    protected static byte[] HmacSHA256(String data, byte[] key) throws Exception {
+        String algorithm="HmacSHA256";
+        Mac mac = Mac.getInstance(algorithm);
+        mac.init(new SecretKeySpec(key, algorithm));
+        return mac.doFinal(data.getBytes("UTF8"));
+    }
+
+    protected static byte[] getSignatureKey(String key, String dateStamp, String regionName, String serviceName) throws Exception {
+        byte[] kSecret = ("AWS4" + key).getBytes("UTF8");
+        byte[] kDate = HmacSHA256(dateStamp, kSecret);
+        byte[] kRegion = HmacSHA256(regionName, kDate);
+        byte[] kService = HmacSHA256(serviceName, kRegion);
+        byte[] kSigning = HmacSHA256("aws4_request", kService);
+        return kSigning;
+    }
+
+    public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, IllegalStateException, UnsupportedEncodingException {
+        String AWS_ACCESS_KEY_ID="MY_ACCESS_KEY";
+        String AWS_SECRET_ACCESS_KEY="MY_SECRET_KEY";
+        String service="monitoring";
+        String host="monitoring.us-east-1.amazonaws.com";
+        String region="us-east-1";
+        String endpoint="https://monitoring.us-east-1.amazonaws.com";
+        String AWS_request_parameters="Action=GetMetricStatistics&Version=2010-08-01";
+        String amz_date = "20181230T125500Z";
+        String date_stamp = "20181230";
+        String canonical_uri = "/";
+        String canonical_querystring = "";
+        String method = "POST";
+        String apiName = "GetMetricStatistics";
+        String content_type = "application/x-amz-json-1.0";
+        String amz_target = "GraniteServiceVersion20100801."+apiName;
+        String canonical_headers = "content-type:" + content_type + "\n" + "host:" + host + "\n" + "x-amz-date:" + amz_date + "\n" + "x-amz-target:" + amz_target + "\n";
+        String signed_headers = "content-type;host;x-amz-date;x-amz-target";
+          String accessKey = AWS_ACCESS_KEY_ID;
+          String accessSecretKey = AWS_SECRET_ACCESS_KEY;
+          String date = "20130806";
+          String signing = "aws4_request";
+        String request_parameters = "{";
+        request_parameters += "    \"Action\": \"GetMetricStatistics\", ";
+        request_parameters += "    \"Namespace\": \"On-PremiseObjectStorageMetrics\",";
+        request_parameters += "    \"MetricName\": \"BucketSizeBytes \",";
+        request_parameters += "    \"Dimensions\": [";
+        request_parameters += "        {";
+        request_parameters += "            \"Name\": \"BucketName\",";
+        request_parameters += "            \"Value\": \"ExampleBucket\"";
+        request_parameters += "        }";
+        request_parameters += "    ],";
+        request_parameters += "    \"StartTime\": 1545884562,";
+        request_parameters += "    \"EndTime\":  1545884662,";
+        request_parameters += "    \"Period\": 86400,";
+        request_parameters += "    \"Statistics\": [";
+        request_parameters += "        \"Average\"";
+        request_parameters += "    ],";
+        request_parameters += "    \"Unit\": \"Bytes\"";
+        request_parameters += "}";
+
+        // String payload_hash = hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
+        // String canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash;
+        // String algorithm = 'AWS4-HMAC-SHA256';
+        // String credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request';
+        // String string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest();
+        // String signing_key = getSignatureKey(secret_key, date_stamp, region, service);
+        // String signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest();
+        // String authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature;
+        // logger.info("signature: {}", getSignatureV4(accessSecretKey, date, region, regionService, signing, request_parameters));
+        try {
+            byte[] signing_key = getSignatureKey(accessSecretKey, date_stamp, region, service);
+            logger.info("signature: {}", encodeToString(signing_key));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
-"""
+//
+// output:
+// [main] INFO com.emc.ecs.s3.sample.GetMetricsRequest - signature: c1391d813f0596e30497d180105f3e2a0defd24f4c5d15d0bdfa22dc905f7e42
+//
